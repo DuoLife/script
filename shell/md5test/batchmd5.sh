e40591ee276832e5d32595ec36349a753047129f
Ming-Linux <vip6ming@126.com>
2015-02-02 17:29:39 +0800

    批量md5重命名，生成压缩包脚本完成

--- shell/md5test/batchmd5.sh
+++ shell/md5test/batchmd5.sh
@@ -18,26 +18,40 @@ else
     `mkdir "$dir/origin"`
     echo "don't have"
 fi
+if [[ -d "$dir/packagethumb" ]]; then
+    echo "have"
+else 
+    `mkdir "$dir/packagethumb"`
+    echo "don't have"
+fi
 if [[ -d "$dir/thumb" ]]; then
     echo "have"
 else 
     `mkdir "$dir/thumb"`
     echo "don't have"
 fi
-#读取表情，并生成md5文件名
-for f in `ls "$originDir/$origin" | grep -E '.*png|jpg'`;
+#读取包缩略图，并生成md5文件名
+for f in `ls "$originDir/packagethumb" | grep -E '.*png'`;
 do 
     if [ -n "$f" ]; then
-    newFile=`md5sum "$f" | grep -oE "^.{32}"`
-    `cp "$originDir/""$f" "$dir/origin/""$newFile"`
+        #newFile=`md5sum "$f" | grep -oE "^.{32}"`
+        `cp "$originDir/packagethumb/$f" "$dir/packagethumb/$dir.pic.thumb"`
+    fi
+done
+#读取表情，并生成md5文件名
+for f in `ls "$originDir/$origin" | grep -E '.*gif|png'`;
+do 
+    if [ -n "$f" ]; then
+        newFile=`md5sum "$originDir/$origin/$f" | grep -oE "^.{32}"`
+        `cp "$originDir/$origin/$f" "$dir/origin/$newFile.pic"`
     fi
 done
 #读取表情缩略图，并生成md5文件名
 for f in `ls "$originDir/$thumb" | grep -E '.*png|jpg'`;
 do 
     if [ -n "$f" ]; then
-    newFile=`md5sum "$f" | grep -oE "^.{32}"`
-    `cp "$originDir/""$f" "$dir/thumb/""$newFile"`
+    newFile=`md5sum "$originDir/$thumb/$f" | grep -oE "^.{32}"`
+    `cp "$originDir/$thumb/$f" "$dir/thumb/$newFile.pic.thumb"`
     fi
 done
 if [[ -d $dir ]];then
